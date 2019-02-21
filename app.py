addb44160f4338dfd117872dfe5568e95d5311c9
sandeshvp <sandeshv@buffalo.edu>
2019-02-21 15:44:49 -0500

    Added post()

--- app.py
+++ app.py
@@ -29,12 +29,34 @@ users = [
 class User(Resource):
 
     def get(self, name):
+        """
+        Get method for retrieving particular user details by specifying the name
+        """
         for user in users:
             if(name == user["name"]):
                 return user, 200
         return "User not found", 404
 
     def post(self, name):
+        """
+        Post method for creating a new user
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name" : name,
+            "age" : args["age"],
+            "occupation" : args["occupation"]
+        }
+        users.append(user)
+        return user, 201
 
     def put(self, name):
 
