6ab8ba5f6ea8f2ee6d066c0aff9aec00756a8bad
sandeshvp <sandeshv@buffalo.edu>
2019-02-21 15:38:38 -0500

    Added get()

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ users = [
 class User(Resource):
 
     def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "User not found", 404
 
     def post(self, name):
 
