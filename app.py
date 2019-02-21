012f3d94e9a27f52258f05186fca9cd51a883b5d
sandeshvp <sandeshv@buffalo.edu>
2019-02-21 15:50:33 -0500

    Added put()

--- app.py
+++ app.py
@@ -59,6 +59,27 @@ class User(Resource):
         return user, 201
 
     def put(self, name):
+        """
+        Put method is used to update details of the user, or create a new one if it does not exist.
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+
+        user = {
+            "name" : name,
+            "age" : args["age"]
+            "occupation" : args["occupation"]
+        }
+        users.append(user)
+        return user, 201
 
     def delete(self, name):
 
